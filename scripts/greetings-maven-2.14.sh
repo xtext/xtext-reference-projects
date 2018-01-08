785f010f1b96244c5606f9dd2189722886e59689
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-08 13:56:09 +0100

    Configure Xtext snapshots
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- scripts/greetings-maven-2.14.sh
+++ scripts/greetings-maven-2.14.sh
@@ -1,2 +1,2 @@
 cd greetings-maven/2.14.0
-mvn -Dtycho-version=1.1.0-SNAPSHOT -s $TRAVIS_BUILD_DIR/settings.xml clean install
+mvn -Dtycho-version=1.1.0-SNAPSHOT -s $TRAVIS_BUILD_DIR/settings.xml -PuseJenkinsSnapshots clean install
