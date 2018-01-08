ce83cd9f8d4f2d56109f209614df85d150a543cf
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-08 15:52:07 +0100

    Maven config
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- scripts/greetings-maven-2.12.sh
+++ scripts/greetings-maven-2.12.sh
@@ -2,7 +2,6 @@ cd greetings-maven/2.12.0
 
 export PROFILES=
 export SETTINGS=
-export SET_TYCHO_VERSION=
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
-mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
--- scripts/greetings-maven-2.13.sh
+++ scripts/greetings-maven-2.13.sh
@@ -1,8 +1,7 @@
 cd greetings-maven/2.13.0
 
-export PROFILES=
-export SETTINGS=
-export SET_TYCHO_VERSION=
+export PROFILES=-Ptycho_snapshots,xtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
-mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
