50a6ea90920114fcaf883381eb62f26c3d2526f3
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-11 09:59:51 +0100

    Added reference project greetings-tycho/2.14.0-J9
    Reference for BREE-9 with Tycho 1.1.0
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ scripts/greetings-tycho-2.14-J9.sh
@@ -0,0 +1,8 @@
+cd greetings-tycho/2.14.0-J9
+
+export PROFILES=-Ptycho_snapshots,xtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export SET_TYCHO_VERSION=-Dtycho-version=1.1.0-SNAPSHOT
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install -U
