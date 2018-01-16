ad2600769228d583c384ab4730b3b0007a29ef4e
Christian Dietrich <christian.dietrich@itemis.de>
2018-01-16 09:24:37 +0100

    fixed scripts
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- scripts/greetings-gradle-2.12.sh
+++ scripts/greetings-gradle-2.12.sh
@@ -1,4 +1,3 @@
 cd greetings-gradle/2.12.0
-gradle wrapper --gradle-version 4.4
 ./gradlew clean build
 
--- scripts/greetings-gradle-2.14-J9.sh
+++ scripts/greetings-gradle-2.14-J9.sh
@@ -1,4 +1,3 @@
 cd greetings-gradle/2.14.0
-gradle wrapper --gradle-version 4.4
 ./gradlew clean build
 
