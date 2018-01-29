b1d6a63c81ce8666986c4775460285e4e6aa18e7
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-29 15:41:36 +0100

    Enable console log
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- scripts/domainmodel-2.14.sh
+++ scripts/domainmodel-2.14.sh
@@ -5,4 +5,4 @@ export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export SET_TYCHO_VERSION=-Dtycho-version=1.1.0-SNAPSHOT
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
-mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
+mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install
