308443693e04034e06aa67b7abc3734749bba394
Karsten Thoms <karsten.thoms@itemis.de>
2018-01-30 12:58:26 +0100

    Enabled Tycho 1.1.0 staging for test
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- scripts/domainmodel-2.14.sh
+++ scripts/domainmodel-2.14.sh
@@ -2,7 +2,7 @@ cd domainmodel/2.14.0/org.eclipse.xtext.example.domainmodel.releng
 
 export PROFILES=-Ptycho_snapshots,xtext_snapshots
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
-export SET_TYCHO_VERSION=-Dtycho-version=1.1.0-SNAPSHOT
+export SET_TYCHO_VERSION=-Dtycho-version=1.1.0
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
 mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install
--- scripts/greetings-tycho-2.14-J9.sh
+++ scripts/greetings-tycho-2.14-J9.sh
@@ -2,7 +2,7 @@ cd greetings-tycho/2.14.0-J9
 
 export PROFILES=-Ptycho_snapshots,xtext_snapshots
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
-export SET_TYCHO_VERSION=-Dtycho-version=1.1.0-SNAPSHOT
+export SET_TYCHO_VERSION=-Dtycho-version=1.1.0
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
 mvn $SET_TYCHO_VERSION $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install -Dtycho.showEclipseLog=true -U
