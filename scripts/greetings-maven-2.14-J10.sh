1a1a6bb1354fb5c5ae60d7c26a50b27c4a973e54
Karsten Thoms <karsten.thoms@itemis.de>
2018-04-17 11:27:16 +0200

    Added greetings-maven-2.14-J10
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ scripts/greetings-maven-2.14-J10.sh
@@ -0,0 +1,7 @@
+cd greetings-maven/2.14.0-J10
+
+export PROFILES=-Ptycho_snapshots,xtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES clean install
