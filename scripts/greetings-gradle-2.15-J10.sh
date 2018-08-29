62b28ec78f3fe01af1edcf928f47df1c5dbb44d4
Karsten Thoms <karsten.thoms@itemis.de>
2018-08-29 17:21:36 +0200

    Added missing scripts for greetings-tycho 2.15
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ scripts/greetings-gradle-2.15-J10.sh
@@ -0,0 +1,3 @@
+cd greetings-gradle/2.15.0-J10/org.eclipse.xtext.example.mydsl.parent
+./gradlew clean build -Dorg.gradle.daemon=false
+
---
+++ scripts/greetings-tycho-2.15.sh
@@ -0,0 +1,7 @@
+cd greetings-tycho/2.15.0-J10
+
+export PROFILES=-Pxtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn -f org.eclipse.xtext.example.mydsl.parent/pom.xml $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install
