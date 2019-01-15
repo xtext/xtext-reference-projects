42d316e54df73c71bb576c22347f60443bf8f075
Holger Schill <Holger.Schill@itemis.de>
2019-01-15 15:52:31 +0100

    [#23] add domainmodel in version 2.17.0

---
+++ scripts/domainmodel-2.17.sh
@@ -0,0 +1,7 @@
+cd domainmodel/2.17.0/org.eclipse.xtext.example.domainmodel.releng
+
+export PROFILES=-Pxtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install
