357ed9c01e24d4c9979277669daa99d6f46202db
Christian Dietrich <christian.dietrich@itemis.de>
2019-09-27 06:33:23 +0200

    splitted execution of integ tests
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ scripts/integrationtests-xtend-examples.sh
@@ -0,0 +1,6 @@
+cd "$(pwd)/integrationtests"
+
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS -Dtycho.showEclipseLog=true clean install -Dtest=XtendExamplesTest
