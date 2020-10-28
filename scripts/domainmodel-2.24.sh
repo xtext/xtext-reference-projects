67b35f6ea57fc62fdba41612060e6708feff4251
Christian Dietrich <christian.dietrich@itemis.de>
2020-10-28 22:01:38 +0100

    added support for gh actions
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- scripts/domainmodel-2.24.sh
+++ scripts/domainmodel-2.24.sh
@@ -1,5 +1,8 @@
 cd domainmodel/2.24.0/org.eclipse.xtext.example.domainmodel.releng
 
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$GITHUB_WORKSPACE}"
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-../../}"
+
 export PROFILES=-Pxtext_snapshots
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
--- scripts/integrationtests-xtext-examples.sh
+++ scripts/integrationtests-xtext-examples.sh
@@ -1,5 +1,6 @@
 cd "$(pwd)/integrationtests"
-
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$GITHUB_WORKSPACE}"
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-../}"
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
