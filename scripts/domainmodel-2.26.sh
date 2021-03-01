13aa955afcb0796c3afc1ec2703d9d0cbcedcbac
Christian Dietrich <christian.dietrich@itemis.de>
2021-03-01 11:35:42 +0100

    [eclipse/xtext#1946] prepare 2.26.0 nightlies
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

---
+++ scripts/domainmodel-2.26.sh
@@ -0,0 +1,10 @@
+cd domainmodel/2.26.0/org.eclipse.xtext.example.domainmodel.releng
+
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$GITHUB_WORKSPACE}"
+TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-../../}"
+
+export PROFILES=-Pxtext_snapshots
+export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
+export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
+
+mvn $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install
