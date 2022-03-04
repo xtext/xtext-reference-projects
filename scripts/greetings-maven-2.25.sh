1eb7fa2115ac62d433d8324a06360293815ba61c
Christian Dietrich <christian.dietrich@itemis.de>
2022-03-04 08:57:55 +0100

    get rid of wired profile and settings part 2
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- scripts/greetings-maven-2.25.sh
+++ scripts/greetings-maven-2.25.sh
@@ -5,7 +5,7 @@ fi
 
 cd greetings-maven/2.25.0
 
-export PROFILES=-Ptycho_snapshots,xtext_snapshots
+export PROFILES=-Ptycho_snapshots
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
