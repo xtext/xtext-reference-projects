5c3729ed5dc748db6fa561cad6c94dfeb54e673f
Christian Dietrich <christian.dietrich@itemis.de>
2022-04-09 07:35:02 +0200

    add EXTRA_ARGS to integ tests too
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- scripts/integrationtests-xtend-examples.sh
+++ scripts/integrationtests-xtend-examples.sh
@@ -6,4 +6,4 @@ TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-../}"
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
-mvn -B $DISABLE_DOWNLOAD_PROGRESS $SETTINGS -Dtycho.showEclipseLog=true clean install -Dtest=XtendExamplesTest
+mvn -B $DISABLE_DOWNLOAD_PROGRESS $SETTINGS -Dtycho.showEclipseLog=true clean install -Dtest=XtendExamplesTest $EXTRA_ARGS
--- scripts/integrationtests-xtext-examples.sh
+++ scripts/integrationtests-xtext-examples.sh
@@ -3,4 +3,4 @@ TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-$GITHUB_WORKSPACE}"
 TRAVIS_BUILD_DIR="${TRAVIS_BUILD_DIR:-../}"
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
-mvn -B $DISABLE_DOWNLOAD_PROGRESS $SETTINGS -Dtycho.showEclipseLog=true clean install -Dtest=XtextExamplesTest
+mvn -B $DISABLE_DOWNLOAD_PROGRESS $SETTINGS -Dtycho.showEclipseLog=true clean install -Dtest=XtextExamplesTest $EXTRA_ARGS
