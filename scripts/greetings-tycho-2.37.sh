dd9fc279c51c50798b98d3eed1641cdafcde1763
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2024-11-26 09:58:14 +0100

    debug log

--- scripts/greetings-tycho-2.37.sh
+++ scripts/greetings-tycho-2.37.sh
@@ -7,4 +7,4 @@ export PROFILES=
 export SETTINGS="-s $TRAVIS_BUILD_DIR/settings.xml"
 export DISABLE_DOWNLOAD_PROGRESS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn 
 
-mvn -B -f org.xtext.example.mydsl.parent/pom.xml $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install $EXTRA_ARGS
+mvn -X -B -f org.xtext.example.mydsl.parent/pom.xml $DISABLE_DOWNLOAD_PROGRESS $SETTINGS $PROFILES -Dtycho.showEclipseLog=true clean install $EXTRA_ARGS
