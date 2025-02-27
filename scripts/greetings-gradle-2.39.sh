903bda31e256bf0f3195fec2ff30a18a9b804a1c
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2025-02-27 07:27:34 +0100

    prepare 2.39 projects

---
+++ scripts/greetings-gradle-2.39.sh
@@ -0,0 +1,3 @@
+cd greetings-gradle/2.39.0/org.xtext.example.mydsl.parent
+./gradlew clean build -Dorg.gradle.daemon=false
+
---
+++ scripts/greetings-gradle-J21-2.39.sh
@@ -0,0 +1,3 @@
+cd greetings-gradle/2.39.0-J21/org.xtext.example.mydsl.parent
+./gradlew clean build -Dorg.gradle.daemon=false
+
